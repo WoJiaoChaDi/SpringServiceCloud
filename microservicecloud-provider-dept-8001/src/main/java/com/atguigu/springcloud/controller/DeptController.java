c885bcfb6188b81595672486a632a88e9de7348f
wojiaochadi <264835229@qq.com>
2020-01-15 14:17:57 +0800

    服务的发现

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,25 @@ public class DeptController
 	public List<Dept> list()
 	{
 		return service.list();
+	}
+
+	//服务的发现
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		//查看Eureka的微服务列表
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+
+		//找一个叫 MICROSERVICECLOUD-DEPT 的微服务
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
 	}
 }
