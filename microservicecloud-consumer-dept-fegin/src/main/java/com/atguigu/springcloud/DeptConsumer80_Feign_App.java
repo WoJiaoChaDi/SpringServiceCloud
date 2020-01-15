d6c1b6a3d36749bb86c9178295ebbf3263b84ab6
wojiaochadi <264835229@qq.com>
2020-01-15 17:09:00 +0800

    consumer-feign  feign实现负载均衡

--- microservicecloud-consumer-dept-fegin/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
+++ microservicecloud-consumer-dept-fegin/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
@@ -3,9 +3,14 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient		//启动Eureka客户端
+//启动Feign
+@EnableFeignClients(basePackages= {"com.atguigu.springcloud"})
+@ComponentScan("com.atguigu.springcloud")
 public class DeptConsumer80_Feign_App
 {
 	public static void main(String[] args)
