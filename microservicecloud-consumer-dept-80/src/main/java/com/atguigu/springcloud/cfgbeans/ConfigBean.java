d30f95032ab672119cb7d56313af6de5c275932a
wojiaochadi <264835229@qq.com>
2020-01-15 16:15:21 +0800

    consumer-80 更换负载均衡的算法

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -24,10 +24,10 @@ public class ConfigBean //boot -->spring   applicationContext.xml --- @Configura
 	{
 		//return new RoundRobinRule();//轮询
 		return new RandomRule();//随机
-		//return new AvailabilityFilteringRule();//先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，还有并发的连接数量超过阀值的服务，然后对剩余的服务列表按照轮询策略进行轮询
-		//return new WeightedResponseTimeRule();//根据平均响应时间计算所有服务的权重，响应时间越快服务权重越大，被选中概率越大
-		//return new RetryRule();//先按照RoundRobinRule的策略获取服务，如果获取服务失败则在指定时间内会进行重试
-		//return new BestAvailableRule();//会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的
+		//return new AvailabilityFilteringRule();//先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，还有并发的连接数量超过阀值的服务，然后对剩余的服务列表按照轮询策略进行访问
+		//return new WeightedResponseTimeRule();//根据平均响应时间计算所有服务的权重，响应时间越快服务被选中的概率越高。刚启动时如果统计信息不足，则使用轮询策略，等统计信息足够，会切换到WeightedResponseTimeRule
+		//return new RetryRule();//先按照RoundRobinRule的策略获取服务，如果获取服务失败则在指定时间内会进行重试，获取可用的服务
+		//return new BestAvailableRule();//会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的服务
 		//return new ZoneAvoidanceRule();//默认规则，复合判断server所在区域的性能和server的可用性选择服务器
 	}
 
