371196a94c48831da9a251698bad6bc6a1cb0d17
wojiaochadi <264835229@qq.com>
2020-01-15 16:10:25 +0800

    consumer-80 更换负载均衡的算法

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,20 @@ public class ConfigBean //boot -->spring   applicationContext.xml --- @Configura
 	{
 		return new RestTemplate();
 	}
+
+	//如果没有配置 IRule 这个类型的bean，则采用默认的负载均衡  轮询
+	@Bean
+	public IRule myRule()
+	{
+		//return new RoundRobinRule();//轮询
+		return new RandomRule();//随机
+		//return new AvailabilityFilteringRule();//先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，还有并发的连接数量超过阀值的服务，然后对剩余的服务列表按照轮询策略进行轮询
+		//return new WeightedResponseTimeRule();//根据平均响应时间计算所有服务的权重，响应时间越快服务权重越大，被选中概率越大
+		//return new RetryRule();//先按照RoundRobinRule的策略获取服务，如果获取服务失败则在指定时间内会进行重试
+		//return new BestAvailableRule();//会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的
+		//return new ZoneAvoidanceRule();//默认规则，复合判断server所在区域的性能和server的可用性选择服务器
+	}
+
 }
 
 //配置项相当于配置了bean对象
