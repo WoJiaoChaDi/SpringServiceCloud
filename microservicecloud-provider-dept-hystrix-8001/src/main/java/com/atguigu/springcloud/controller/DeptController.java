8f9db49f9ca163e2c726d6b2ed0fc6c4a94d7478
wojiaochadi <264835229@qq.com>
2020-01-16 11:11:07 +0800

    hystrix-8001  熔断机制（当异常的时候，返回一个 符合预期的、可处理的备选响应）

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -26,9 +27,18 @@ public class DeptController
 	}
 
 	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	//如果出现异常，像调用者返回一个 符合预期的、可处理的备选响应
+	@HystrixCommand(fallbackMethod = "processHystrix_Get")
 	public Dept get(@PathVariable("id") Long id)
 	{
-		return service.get(id);
+		Dept dept = this.service.get(id);
+
+		//模仿出现异常，进行熔断
+		if (null == dept) {
+			throw new RuntimeException("该ID：" + id + "没有没有对应的信息");
+		}
+
+		return dept;
 	}
 
 	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
@@ -55,5 +65,13 @@ public class DeptController
 					+ element.getUri());
 		}
 		return this.client;
+	}
+
+	//hystrix 回调函数
+	public Dept processHystrix_Get(@PathVariable("id") Long id)
+	{
+		//如果出现问题，则返回一个符合预期的内容（这里是Dept）
+		return new Dept().setDeptno(id).setDname("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand")
+				.setDb_source("no this database in MySQL");
 	}
 }
